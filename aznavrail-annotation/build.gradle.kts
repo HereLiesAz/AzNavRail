1f39d47ecb5cd70f6e393287e51f80aef26a4751
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-02-15 22:55:50 -0600

    Update build.gradle.kts

--- aznavrail-annotation/build.gradle.kts
+++ aznavrail-annotation/build.gradle.kts
@@ -1,7 +1,22 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.2.21"
+    id("maven-publish") // REQUIRED FOR JITPACK
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+// JITPACK PUBLISHING SCRIPT
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["java"])
+                groupId = "com.github.HereLiesAz.AzNavRail"
+                artifactId = "aznavrail-annotation"
+                version = "1.0" // JitPack overrides this, but Gradle needs a value
+            }
+        }
+    }
 }
