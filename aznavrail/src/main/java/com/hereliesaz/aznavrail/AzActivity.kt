fb463866991c22372937855c1a53b763b2a25574
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-02-19 10:38:50 -0600

    Update AzActivity.kt

--- aznavrail/src/main/java/com/hereliesaz/aznavrail/AzActivity.kt
+++ aznavrail/src/main/java/com/hereliesaz/aznavrail/AzActivity.kt
@@ -1,12 +1,18 @@
+// aznavrail/src/main/java/com/hereliesaz/aznavrail/AzActivity.kt
 package com.hereliesaz.aznavrail
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.hereliesaz.aznavrail.annotation.Az
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.hereliesaz.aznavrail.model.AzDockingSide
 
 abstract class AzActivity : ComponentActivity() {
 
     abstract val graph: AzGraphInterface
+
+    // The escape hatch. Update this state to trigger a dynamic reconfiguration.
+    open val dynamicDockingSide: MutableState<AzDockingSide?> = mutableStateOf(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
