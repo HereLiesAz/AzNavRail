aea6d2befcec636a413ca7dd7dbfc24914afaeea
Az <hereliesaz@gmail.com>
2026-07-16 00:10:36 -0500

    aqswdfh

--- figma-plugin/figma-plugin-temp/vite.config.ts
+++ figma-plugin/figma-plugin-temp/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
+  plugins: [viteSingleFile()],
   build: {
     target: "esnext",
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    brotliSize: false,
     emptyOutDir: false,
     rollupOptions: {
       input: {
+        ui: "src/ui.html",
         code: "src/code.ts",
       },
       output: {
